6290df0e840cfa8631d5af724ba82d9c04ad13f1
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:26:42 -0500

    Strings en java

---
+++ Cadenas.java
@@ -0,0 +1,12 @@
+public class Cadenas{
+	public static void main(String [] args){
+	
+		String palabra= "Palabra";
+		String concatenacion=palabra+" Concatenada";
+
+		System.out.println("La palabra tiene "+palabra.length()+"letras");
+		System.out.println("Al juntar las cadenas tenemos: "+concatenacion);
+	       	System.out.println("Si usamos comillas al sumar 2 y 5 dará: "+"2"+"5");
+		System.out.println("Si no las usamos será: "+(2+5));	
+	}
+}

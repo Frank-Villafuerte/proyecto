e0787015b8b7140bc5dec3d89f421b060e0601e6
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:33:20 -0500

    Clase Math de java

---
+++ Matematica.java
@@ -0,0 +1,11 @@
+public class Matematica{
+	public static void main(String[] args){
+		
+
+		System.out.println("El menor entre 4 y 9 es: "+Math.min(4,9));//usamos clase math
+									   	
+		System.out.println("El valor absoluto de -45 es " + Math.abs(-45));// abs halla el valor absoluto de su argumento
+		System.out.println("Si escogiera al azar un numero menor a 50 sería: "+Math.random()*50+1);	
+	}
+}
+

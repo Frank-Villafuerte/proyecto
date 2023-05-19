551e5ed6493d3b6911161923577e6baae17e4b9e
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:08:00 -0500

    Tipos de dato

---
+++ TipoDato.java
@@ -0,0 +1,9 @@
+public class TipoDato{
+	public static void main(String[] args){
+		
+		int numero;//tipo de dato que solo almacena enteros
+		boolean verdad;// este es un dato primitivo, verdadero o falso
+		char letra;// dato primitivo, almacena letras
+		String palabra;// dato no primitivo, pues almacena varios char unificados
+	}
+}

d92c6548c944d01e2bd67cc6c297c6bf42774b19
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 21:48:35 -0500

    Output de java

---
+++ Salida.java
@@ -0,0 +1,8 @@
+public class Salida{
+	public static void main(String[] args){
+		System.out.println("HOLA");//Podemos imprimir texto. Debe colocarse entre comillas
+		System.out.println("5"); //Los numeros pueden ir con o sin comillas
+		System.out.println(5);// En este caso mostrará 5 igual que el anterior
+		System.out.println(2+3);//Podemos colocar operaciones matematicas
+	}
+}

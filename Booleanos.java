7229bdd366e35cb38b705fcd0bfb06a7f78d0b8a
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:40:29 -0500

    Booleans en Java

---
+++ Booleanos.java
@@ -0,0 +1,11 @@
+public class Booleanos{
+	public static void main(String[] args){
+		boolean verdadero=true;//Se puede declarar directamente el valor
+		boolean falso=false;
+
+		boolean comparacion=5<4;//tambien se puede definir mediante una comparacion
+
+		System.out.println(verdadero+"-" + falso);
+		System.out.println("La comparacion es "+comparacion);
+	}
+}

243d79cea2611c7b2377523d589361c485f397e6
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:52:25 -0500

    Switch en java

---
+++ Switch.java
@@ -0,0 +1,32 @@
+public class Switch{
+	public static void main(String[] args){
+		
+		int numero=5;
+
+		switch(numero){
+			case 1:
+				System.out.print("Uno");
+				break;
+			case 2:	
+				System.out.print("Dos");
+				break;
+		
+			case 3:
+				System.out.print("Tres");
+				break;
+			case 4:
+				System.out.print("Cuatro");
+				break;
+			case 5:
+				System.out.print("Cinco");
+				break;
+			case 6:
+				System.out.print("Seis");
+				break;
+			case 7:
+				System.out.print("Siete");
+				break;
+		}
+	}
+}
+

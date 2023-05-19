0c377f900a14cfa90106bd4da536a74cf32ebf28
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:00:33 -0500

    Declarar variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+ public class Variables{
+	public static void main(String[] args){    
+		
+
+		String nombre="pedro"; //Variable cadena con valor Pedro
+		int edad=15; //int guarda numeros enteros
+		char inicial='P'; //char puede almacenar un caracter como P
+		float estatura=1.64; // Float puede tener decimales, pero en poca cantidad
+	}
+}
+
+
+
+
+    

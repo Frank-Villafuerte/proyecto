78a17ee90ce4b09a4b2aea1068d1afba7105ba9e
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:44:22 -0500

    If-else en java

---
+++ SiEntonces.java
@@ -0,0 +1,11 @@
+public class SiEntonces{
+	public static void main(String[] args){
+
+		if(5<1){
+			System.out.println("5 es menor que 1");//Si se cumple la condicion se ejecuta este comando
+		}
+		else{
+			System.out.println("5 es mayor que 1");//De no cumplir la condicion se ejecuta este en lugar del anterior
+		}
+	}
+}

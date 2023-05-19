a273d326dfd24c70d4557bd836d57eb1c8c9e149
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 23:34:31 -0500

    Metodos en java

---
+++ Metodos.java
@@ -0,0 +1,11 @@
+public class Metodos{
+	public static void main(String[] args){
+	
+
+		imprimeHola();
+		imprimeHola();
+	}
+	public static void imprimeHola(){//Este codigo puede ejecutarse con llamar al metodo, asi se puede reutilizar con mayor facilidad
+		System.out.println("Hola");
+	}
+}

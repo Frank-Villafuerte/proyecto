40fb31d8d9e35134b516422ea50a5d9b7629e771
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 22:18:14 -0500

    Operadores matematicos

---
+++ Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores{
+	public static void main(String[] args){
+
+		int suma=5+5;
+		int multiplicacion=2*4;
+		int division=5/2;
+		int modulo=7%4;
+		
+		//El valor de las variables será igual al resultado de las operaciones
+
+		System.out.println(suma);
+		System.out.println(multiplicacion);	
+		System.out.println(division);
+		System.out.println(modulo);
+	}
+}

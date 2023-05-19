37a9763656a35ed699b1d9262c1c179bc6c5b24a
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 21:53:08 -0500

    Comentarios

---
+++ Comentarios.java
@@ -0,0 +1,12 @@
+public class Comentarios{
+	public static void main(String[] args){    
+		//Esto es un comentario, no afecta en nada al codigo
+		/*Este es otro comentario, 
+		 tiene la ventaja de no finalizar hasta encontrar el simbolo asterisco y diagonal*/
+	}
+}
+
+
+
+
+

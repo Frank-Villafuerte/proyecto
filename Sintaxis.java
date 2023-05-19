a63ebad04b1d37452ba7f4077780c0155288a70d
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 21:39:46 -0500

    Repaso de sintaxis

---
+++ Sintaxis.java
@@ -0,0 +1,6 @@
+public class Sintaxis{ //Nombre de la clase
+	public static void main(String[] args){    //Metodo principal al ser ejecutado
+		System.out.println("Hola Mundo");//Instruccion para imprimir en consola dentro de main()
+	}
+}
+

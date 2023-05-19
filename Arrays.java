6ff799a44beddf5cf33e7d9c385e26fe80956795
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2023-05-18 23:28:49 -0500

    Arrays simples y multidimensionales en java

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays{
+	public static void main(String[] args){
+		int[] simple={1,2,3,4,5,6,7,8,9,10};
+		int[][] multidimensional={{1,3,5,7,9},{2,4,6,8,10}};
+
+
+		System.out.println("Un numero de la lista normal es :"+simple[0]); //Se debe especificar la posicion que ocupa el elemento deseado
+
+		System.out.println("Un numero de la lista multidimensional es "+multidimensional[1][4]);// Se especifica el orden del array simple y luego la posicion del elemento deseado
+	}
+}
+
